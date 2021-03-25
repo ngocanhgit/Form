a0dc2d75ca836aea71a35196c42b606e309a7b8f
ANH <you@example.com>
2021-03-25 13:54:48 +0700

    anh

---
+++ login.php
@@ -0,0 +1,12 @@
+<head>
+    <tile>Trang đăng nhập nè</tile>
+    <meta charset="utf-8">
+</head>
+
+<form action="#" method="post">
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text"  name="uname" required>
+  </div>
+  
+</form>
